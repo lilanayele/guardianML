14539cf26b924a4b88176affb014e317cbee4f67
lilanayele <LilanG@amitydubai.ae>
2026-07-02 10:42:46 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,5 +1,4 @@
 import joblib
-import pandas as pd
 
 rf = joblib.load("models/random_forest.pkl")
 encoders = joblib.load("models/encoders.pkl")
@@ -7,26 +6,25 @@ encoders = joblib.load("models/encoders.pkl")
 
 def predict(sample_df):
 
-    # REMOVE LABEL + ID
+    # REMOVE LABEL IF PRESENT
     if "attack_detected" in sample_df.columns:
         sample_df = sample_df.drop(columns=["attack_detected"])
 
+    # REMOVE ID COLUMN
     if "session_id" in sample_df.columns:
         sample_df = sample_df.drop(columns=["session_id"])
 
-    #ENCODE CATEGORICAL FEATURES
-for col, encoder in encoders.items():
-    if col in sample_df.columns:
+    # ENCODE CATEGORICAL COLUMNS SAFELY
+    for col, encoder in encoders.items():
+        if col in sample_df.columns:
+            sample_df[col] = sample_df[col].astype(str)
 
-        # Convert everything to string first
-        sample_df[col] = sample_df[col].astype(str)
+            sample_df[col] = sample_df[col].apply(
+                lambda x: encoder.transform([x])[0]
+                if x in encoder.classes_
+                else -1
+            )
 
-        # SAFE mapping
-        sample_df[col] = sample_df[col].apply(
-            lambda x: encoder.transform([x])[0]
-            if x in encoder.classes_
-            else -1   # unknown category
-        )
     # PREDICT
     pred = rf.predict(sample_df)[0]
     proba = rf.predict_proba(sample_df)[0][1]
