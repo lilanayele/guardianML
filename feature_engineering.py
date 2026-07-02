8afc115fbd1ecbbb371692117521c416ce23d370
lilanayele <LilanG@amitydubai.ae>
2026-07-02 09:50:46 +0300

    Delete feature_engineering.py

--- feature_engineering.py
+++
@@ -1,6 +0,0 @@
-def create_features(df):
-    # simple safe features (works for most datasets)
-    if "bytes" in df.columns and "duration" in df.columns:
-        df["packet_rate"] = df["bytes"] / (df["duration"] + 1)
-
-    return df
