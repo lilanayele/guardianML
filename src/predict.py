5ab373eff3c5262766e58a9a85eab9c3d397604d
lilanayele <LilanG@amitydubai.ae>
2026-07-02 10:26:15 +0300

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -2,18 +2,24 @@ import joblib
 import pandas as pd
 
 rf = joblib.load("models/random_forest.pkl")
-iso = joblib.load("models/isolation_forest.pkl")
 
+target_col = "attack_detected"
 
 def predict(sample_df):
 
+    # REMOVE TARGET IF IT EXISTS
+    if target_col in sample_df.columns:
+        sample_df = sample_df.drop(columns=[target_col])
+
+    # PREDICT
     pred = rf.predict(sample_df)[0]
+    proba = rf.predict_proba(sample_df)[0][1]
 
-    anomaly = iso.predict(sample_df)[0]
+    risk = int(proba * 100)
 
-    prob = rf.predict_proba(sample_df)[0]
-    risk = int(max(prob) * 100)
-
-    status = "Anomaly Detected" if anomaly == -1 else "Normal"
+    if pred == 1:
+        status = "⚠️ Attack Detected"
+    else:
+        status = "✅ Normal"
 
     return pred, status, risk
